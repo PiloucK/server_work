c9348dfc78139aad5d27108546a86fd1f1cfd0b4
clkuznie <kuzniewycz.clement@gmail.com>
2020-08-19 15:11:48 +0200

    .

--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         'kX;jEIv-SNkQ4MJ?Z({v^$j>e$/}eFzOg_&/7@3|Om~!+|M&=>i#Q4m1I6x|>2 ~');
+define('SECURE_AUTH_KEY',  'T9:Eu yY--Yl.mc.QT#z@~U4!0 ,-YN+D_HVBh6#uJRQ)C.Ey084QwfUqshA:c-t');
+define('LOGGED_IN_KEY',    '.+JGLQ -!j!ke5/C9TT+-*ihQ+$!+WGT7251z=T-3wFpq*IzWg2.<}Vca#+R~?kt');
+define('NONCE_KEY',        'd%=boJQ&/zY(`Qz3Fj Z?+[gD-LO(0Sx,#N=4O/UZm#o{NC)tGx1-+%vav^p?;_A');
+define('AUTH_SALT',        '|80kLa{8vltX&:X-l!D #^AC;43Dh>WOx)52fs.x[$Ti:y@#?D@SOAS+ef6N_!rX');
+define('SECURE_AUTH_SALT', 'ollRy`ULZBbG*UBgo8xa>d?IYw@DF/CY!McvJ1?r;&eq}`~+Y|3E+n/c3z%EzZcj');
+define('LOGGED_IN_SALT',   'T(:UX?_#rUbeME=+&MSIKD%qtu6BOrl+.o,@NrV6sBT# m9oyG&?8S3L|++~ld}|');
+define('NONCE_SALT',       'Ial?`J4vVNe*E$|?_q1=Y>k!fA8LEY{&vXo]U5?pFdi}tT{}Pc_0(LNr]S~Z(+!2');
 
 /**#@-*/
 
